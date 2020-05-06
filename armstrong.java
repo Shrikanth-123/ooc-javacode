b5a2ae5565532fc06ccb989abd6ec9fff0d493ac
Shrikanth-123 <53474698+Shrikanth-123@users.noreply.github.com>
2020-05-06 22:38:25 +0530

    Update armstrong.java

--- armstrong.java
+++ armstrong.java
@@ -1,1 +1,29 @@
 
+public class Armstrong {
+
+	public static void main(String[] args) {
+		
+		int num, Num1=0, rem=0, res= 0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Ënter the number");
+		num=sc.nextInt();
+        Num1= num;
+		while(Num1!=0)
+		{
+			rem=Num1%10;
+			res=res+rem*rem*rem;
+			Num1=Num1/10;
+			
+		}
+        if(res == num)
+        {
+            System.out.println(num + " is an Armstrong number");
+        }
+        else 
+        {
+            System.out.println(num+ " is not an Armstrong number");
+           
+        }
+        
+	}
+}
